dce539757b8a136ce59bd59092c8352e5ca87821
luisferreira88 <miguelituh@hotmail.com>
2023-08-08 15:35:55 +0100

    4 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import './style.css';
 
 export default function App() {
   const courseExample: Course = {
-    title: 'CURSO XPTO',
+    title: 'Pathway XPTO',
     description:
       'ad asjndkjd asd asd asd sad asd asd sad asd asd asd sad asd sad sad sad asd as das dsad asd asd asd sad asd sad asd sad asd asd sad asd asd sad asd asd asd asd asd asd asd asd asd asd asd asd asd asd sad asd asd asd bd jkasbd kasjd asd asd',
     imageUrl:
-      'https://cdn.pixabay.com/photo/2016/08/09/17/52/instagram-1581266_640.jpg',
+      'https://cdn.pixabay.com/photo/2016/08/09/17/52/instagram-1581266_640.jpg'
   };
 
   return (
